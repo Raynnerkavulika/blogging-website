855cd48f1444b1ac1c339c0af41c82b5f604e7a8
Altoid <102745236+Raynnerkavulika@users.noreply.github.com>
2024-07-24 18:47:42 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php  
+$dbname = "mysql:host=localhost;dbname=blogsite";
+$username = "root";
+$password = "";
+
+$conn = new PDO($dbname,$username,$password);
---
+++ scrit.js
@@ -0,0 +1,6 @@
+let header = document.querySelector('#menu-btn');
+document.querySelector('active').onclick = () =>{
+
+}
+
+windows.onscroll
